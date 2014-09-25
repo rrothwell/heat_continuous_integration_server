092a2a586dc93ae3f4c2277307a11645afe009a8
Richard Rothwell <rrothwell@v3.org.au>
2014-09-25 17:58:51 +1000

    RR: fixed apt-get line by adding -y. Jenkins works now.

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -34,8 +34,8 @@ echo "Installing Jenkins. "
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get -y update
+sudo apt-get install -y jenkins
 
 echo "Configuring Jenkins. "
 
